632065bf3240e35b782e141c5de1811e9b9a7662
Ravi Kumar <ravikumargrk@hotmail.com>
2025-07-15 08:09:09 +0530

    Update go.cmd

--- go.cmd
+++ go.cmd
@@ -4,5 +4,5 @@ set packages=%USERPROFILE%/packages
 set diffs=Z:\EU\AUTH\SMV\diffxlsx
 set payloads=Z:\EU\AUTH\SMV\payloads
 
-python "C:\Users\E161262\OneDrive - Mastercard\Documents\TASKS\go\go.py" %*
-if exist "C:\Users\E161262\OneDrive - Mastercard\Documents\TASKS\go\go_temp.cmd" call "C:\Users\E161262\OneDrive - Mastercard\Documents\TASKS\go\go_temp.cmd"
+python "go.py" %*
+if exist "go_temp.cmd" call "go_temp.cmd"
