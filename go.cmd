b4a29c2777a3cacb554a12cf7da5cfdd0c17c9c1
Ravi Kumar <ravikumargrk@hotmail.com>
2025-07-15 08:09:30 +0530

    Update go.cmd

--- go.cmd
+++ go.cmd
@@ -1,8 +1,6 @@
 @echo off
 
 set packages=%USERPROFILE%/packages
-set diffs=Z:\EU\AUTH\SMV\diffxlsx
-set payloads=Z:\EU\AUTH\SMV\payloads
 
 python "go.py" %*
 if exist "go_temp.cmd" call "go_temp.cmd"
